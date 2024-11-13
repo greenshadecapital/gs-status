05bf50b2a3912a55277413fc6b2a24f2aa67d0c1
phil616 <phil616@163.com>
2024-11-13 16:08:50 +0800

    update: update UI

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
         <div>
           <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">
             <Text size="xl" span>
-              🕒
+              
             </Text>
             <Text
               size="xl"
@@ -32,7 +32,7 @@ export default function Header() {
               variant="gradient"
               gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
             >
-              UptimeFlare
+              Green Shade Capital Status
             </Text>
           </a>
         </div>
